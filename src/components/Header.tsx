41d5e97ce9915d2c403ff2831a1732d25cd4953b
Gedeon <fobadeffo@gmail.com>
2025-10-06 00:42:03 +0100

    preview updates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Menu, X } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import logo from '../../public/logo.png';
+import logo from '../../public/logo.jpg';
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
