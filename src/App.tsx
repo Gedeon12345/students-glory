713b8c6dbf615858de3b1e0f8314b95f0ca5bb17
Gedeon <fobadeffo@gmail.com>
2025-10-06 01:44:15 +0100

    google adds

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8251279282627098" crossorigin="anonymous"></script>
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
