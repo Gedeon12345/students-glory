3d1885bed422b496e01e55690c61c778ce42d45e
Gedeon <fobadeffo@gmail.com>
2025-10-06 14:44:23 +0100

    google adds

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,11 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-    <Analytics/>
       <Toaster />
       <Sonner />
       <BrowserRouter>
         <Routes>
+          <Analytics />
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
